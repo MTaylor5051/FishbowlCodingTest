6da5bed060d8d566985820e28bc27156b778d16a
mita6978 <mita6978@colorado.edu>
2021-11-04 00:14:49 -0500

    Added Toastr for API Errors

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 
 import { HttpInterceptorService } from './http-interceptor/http-interceptor.service';
 
@@ -8,7 +11,9 @@ import { HttpInterceptorService } from './http-interceptor/http-interceptor.serv
   declarations: [],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true }]
 })
--- src/app/core/http-interceptor/http-interceptor.service.ts
+++ src/app/core/http-interceptor/http-interceptor.service.ts
@@ -1,28 +1,43 @@
 import { Injectable } from '@angular/core';
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 
 import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs/internal/observable/throwError';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpInterceptorService implements HttpInterceptor  {
 
-  constructor() { }
+  constructor(private _toastr: ToastrService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
     const token: string = '80e752d7-4ae6-471e-adcd-9b21f76acf35';
 
-    request = request.clone({ url: `https://stageapi.fbowlapp.com${request.url}` });
+    const requestAmended = request.clone(
+      {
+        url: `https://stageapi.fbowlapp.com${request.url}`,
+        headers: request.headers.set('session-key', token)
+      });
 
-    if (token) {
-        request = request.clone({ headers: request.headers.set('session-key', token) });
-    }
+    return next.handle(requestAmended).pipe(
+      // map((event: HttpEvent<any>) => {
+      //     if (event instanceof HttpResponse) {
+      //         console.log('event--->>>', event);
+      //     }
+      //     return event;
+      // }),
+      catchError((errorResponse: HttpErrorResponse) => {
+          const reason = errorResponse && errorResponse.error && errorResponse.error.message || 'Error sending or receiving data';
 
-    console.log(request)
+          this._toastr.error(`${errorResponse.status} - ${reason}`, 'Uh-oh!');
 
-    return next.handle(request);
+          return throwError(errorResponse);
+      }));
 }
 
 }
+
